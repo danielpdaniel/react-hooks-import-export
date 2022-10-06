a6f8ae42ee2d6f639ce5961595cb980e4cc8d6da
danielpdaniel <artofjquinn@gmail.com>
2022-10-06 16:04:23 -0400

    commit tree 7d342ef2d04cd932b55642535935fd57884f69cc

--- src/ColoradoStateParks.js
+++ src/ColoradoStateParks.js
@@ -1,8 +1,13 @@
 import React from "react";
 import howManyParks from "./parks/howManyParks";
+import * as RockyMountainExports from "./parks/RockyMountain";
 
 function ColoradoStateParks() {
   howManyParks(); // => "42 parks!"
+  console.log(RockyMountainExports.trees);
+  RockyMountainExports.wildlife();
 
   return <h1>Colorado State Parks!</h1>;
 }
+
+export default ColoradoStateParks
